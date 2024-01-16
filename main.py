6b77c5f5e19eeb37302d121cb64b3474feb82f39
Aeziak <aeziak@gmail.com>
2024-01-16 21:34:37 +0100

    Resize videos to 1920x1080, may ask video final format later as an input or a parameter

--- main.py
+++ main.py
@@ -9,14 +9,19 @@ for video in glob.glob("./videos/*.mp4"):
     match = re.search(';(.*);', video)
     if (match is not None):
         name = match.group(1)
-    if name == "" or (match is None):
+    else:
         name = "default"
-    returnedVideo = VideoFileClip(video).fx(vfx.fadein, 1).fx(vfx.fadeout, 1)
-    image = ImageClip("./nameplates/" + name + ".png").set_duration(3).set_pos(("right","top")).fx(vfx.fadein, 1).fx(vfx.fadeout, 1)
+
+    # Create video clip from file and apply resizing and FX
+    returnedVideo = VideoFileClip(video, target_resolution=(1080, 1920)).fx(vfx.fadein, 1).fx(vfx.fadeout, 1)
+    # Create the nameplate image from files with {name} as file name
+    image = ImageClip("./nameplates/" + name + ".png").set_duration(3).set_pos(("left","top")).fx(vfx.fadein, 1).fx(vfx.fadeout, 1)
+    image = image.resize(0.5)
+    
     returnedVideo = CompositeVideoClip([returnedVideo, image])
     clips.append(returnedVideo)
-    print(video)
 
+# Concatenate all the video clips together
 combined = concatenate_videoclips(clips)
 length = len(glob.glob("./output/*"))
 combined.write_videofile("./output/" + str(length) + ".mp4")
